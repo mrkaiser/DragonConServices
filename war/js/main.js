a0254c115553df882e838857144fd5ebae381cce
mrkaiser <kaiserollofdoom@gmail.com>
2013-06-02 18:05:23 -0400

    Added some extra UI elements for Vendors and Speakers. They do not currently post to any significant URL

--- war/js/main.js
+++ war/js/main.js
@@ -1,6 +1,47 @@
-function TracksCtrl($scope, $http){
+var myApp = angular.module('myApp',[]);
+
+myApp.factory('Data',function(){
+	return {
+		vendor: {
+			company_name: "",
+			product_pod:"",
+			product_description:""
+		},
+		speaker: {
+			"bio": "",
+		   	"first_name": "",
+		   	"last_name": "",
+		   	"display_name": "",
+		   	"plusone_url": "",
+		   	"thumbnail_url": "",
+		   	"user_id": "",
+		   	"speaker_id": ""
+		}
+	};
+});
+
+function TracksCtrl($scope, $http,Data){
 
 	$http.get('_ah/api/models/v1/tracks/get').success(function(data){
 		$scope.tracks = data;
 	});
+	$scope.data = Data;
+}
+
+function VendorCtrl($scope, $http,Data){
+	$http.get('_ah/api/models/v1/tracks/get').success(function(data){
+		$scope.tracks = data;
+	});
+
+	$scope.save = function(){
+		$http.post('/someurl',$scope.data.vendor)
+	};
+	$scope.data = Data;
+}
+
+function SpeakerCtrl($scope,$http,Data){
+	$scope.save = function(){
+		console.log("blah")
+		$http.post('/speakerurl',$scope.speaker)
+	}
 }
